aa9f61f62a8a5a0d573f2a541693127919b33bee
uwuflanny <gianlumiglia@gmail.com>
2021-06-16 12:10:44 +0200

    added gui, added data insertion, ddl fix

---
+++ FormulaDB_GUI/FormulaDB_GUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+
+namespace FormulaDB_GUI
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("item.txt")) {
+                string item = sr.ReadToEnd().Replace("<tr>", "").Replace("</tr>", "").Replace("<td>","x").Replace("</td>","x");
+                string[] items = item.Split('x');
+                foreach(string s in items)
+                {
+                    MessageBox.Show(s);
+                }
+            }
+        }
+    }
+}
