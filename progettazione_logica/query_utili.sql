f34f6a4d38857295cc384db46c23a6ef49117c34
uwuflanny <gianlumiglia@gmail.com>
2021-06-19 17:56:53 +0200

    asdasdadsd

--- progettazione_logica/query_utili.sql
+++ progettazione_logica/query_utili.sql
@@ -23,15 +23,6 @@ group by pilota
 */
 
 
-/*
--- classifica per ogni gara di un certo campionato
-select gara, pilota, posizione, scuderia, circuito
-from (riepilogo join info_gara on riepilogo.gara = info_gara.ID join risultati_gara on risultati_gara.ID_riepilogo = riepilogo.ID) 
-where campionato = 1
-order by gara, posizione 
-*/
-
-
 
 /* -- punteggi solo con le posizioni dalle gare
 select pilota, sum(punteggio) as punteggio from (
@@ -45,6 +36,38 @@ order by punteggio desc
 
 
 
+
+
+
+
+/*
+
+select res1.scuderia, punteggio + IFNULL(girimigliori, 0) as punteggio from 
+	( select scuderia, sum(punteggio) as punteggio
+	from (riepilogo join info_gara on riepilogo.gara = info_gara.ID join risultati_gara on risultati_gara.ID_riepilogo = riepilogo.ID join punti_posizione on risultati_gara.posizione = punti_posizione.posizione)
+	where campionato = 1 
+	group by scuderia) as res1
+    
+    left join 
+    
+    (select scuderia, count(*) as girimigliori from ( -- migliore giro per scuderia
+		select *
+		from (
+			select ID_riepilogo, gara, pilota ,scuderia ,min(tempo) as best
+			from giro join riepilogo on riepilogo.ID = giro.ID_riepilogo
+			group by gara, pilota
+			order by gara, best ) as res
+		group by gara ) as res
+	group by scuderia) as res2
+    
+    on res1.scuderia = res2.scuderia
+	order by punteggio
+
+*/
+
+
+
+/*
 select res1.pilota, punteggio + IFNULL(girimigliori, 0) as punteggio from 
 
 	(select pilota, sum(punteggio) as punteggio from (
@@ -68,7 +91,7 @@ select res1.pilota, punteggio + IFNULL(girimigliori, 0) as punteggio from
 	group by pilota) as res2
     
     on res1.pilota = res2.pilota
-
+*/
 
 
 
